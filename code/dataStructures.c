042ebd54c319642e6538c4399f10a6e2bab35490
Christopher Stoll <stollcri@gmail.com>
2014-01-24 22:30:38 -0500

    Continue enqueue dequeue work

--- code/dataStructures.c
+++ code/dataStructures.c
@@ -19,13 +19,66 @@ struct list {
 };
 
 /**
+ * Simple queue structure
+ */
+struct queue {
+	struct node *head;
+	struct node *tail;
+};
+
+/**
  * Node creation function
  */
-struct node *createNode(int vertex) {
-	struct node *newNode = (struct node*) malloc(sizeof(struct node));
+struct node *newNode(int vertex) {
+	struct node *thisNode = (struct node*) malloc(sizeof(struct node));
 
-	newNode->vertex = vertex;
-	newNode->next = NULL;
+	thisNode->vertex = vertex;
+	thisNode->next = NULL;
 
-	return newNode;
+	return thisNode;
+}
+
+/**
+ * Queue creation function
+ */
+struct queue *newQueue() {
+	struct queue *newQueue = (struct queue*) malloc(sizeof(struct queue));
+
+	newQueue->head = NULL;
+	newQueue->tail = NULL;
+
+	return newQueue;
+}
+
+/**
+ * Enqueue a node
+ */
+void enqueue(struct queue *thisQueue, int vertex) {
+	struct node *thisNode = newNode(vertex);
+
+	if (thisQueue->head == NULL) {
+		thisQueue->head = thisNode;
+		thisQueue->tail = thisNode;
+
+		printf("NULL \n");
+	} else {
+		// TODO: finish this
+		printf("NOT NULL \n");
+	}
+}
+
+/**
+ * Dequeue a node
+ */
+struct node *dequeue(struct queue *thisQueue) {
+	// TODO: implement this
+	struct node *thisNode = newNode(0);
+
+	if (thisQueue->head == NULL) {
+		printf("NULL \n");
+	} else {
+		printf("NOT NULL \n");
+	}
+
+	return thisNode;
 }
