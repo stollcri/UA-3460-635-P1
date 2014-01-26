8f0f8e0a4614d7c27cafb9b27da533ca89110343
Christopher Stoll <stollcri@gmail.com>
2014-01-26 11:07:03 -0500

    Extract class linkedList

---
+++ code/linkedList.c
@@ -0,0 +1,18 @@
+/**
+ * Linked List Data Structure
+ * Christopher Stoll, 2014
+ */
+
+#ifndef INCLUDE_LINKEDLIST
+#define INCLUDE_LINKEDLIST
+
+#include "node.c"
+
+/**
+ * Simple list structure
+ */
+struct list {
+	struct node *head;
+};
+
+#endif
