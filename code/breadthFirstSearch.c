e3768a1a513cfc3077cd902f7f5e317132fe97b6
Christopher Stoll <stollcri@gmail.com>
2014-01-26 13:02:34 -0500

    BFS update

--- code/breadthFirstSearch.c
+++ code/breadthFirstSearch.c
@@ -8,6 +8,13 @@
 #include "graph.c"
 #include "queue.c"
 
+#include <stdio.h>
+
+#define MAX_PATH_SIZE 256
+
 struct list bfs(struct graph* thisGraph, int start, int end) {
-	struct node* startNode = thisGraph->adjacencyList[start].head;
+	struct list *path = (struct list*) malloc(sizeof(struct list) * MAX_PATH_SIZE);
+	struct node *startNode = thisGraph->adjacencyList[start].head;
+
+	return *path;
 }
