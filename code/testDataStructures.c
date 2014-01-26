ce38a669199577028cded1d812de9a7aad898c39
Christopher Stoll <stollcri@gmail.com>
2014-01-26 11:53:44 -0500

    Updated data structure test

--- code/testDataStructures.c
+++ code/testDataStructures.c
@@ -3,10 +3,25 @@
 #include "linkedList.c"
 #include "queue.c"
 
-int main(int argc, char const *argv[]) {
+void testQueue() {
 	struct queue *myQueue = newQueue();
+
 	enqueue(myQueue, 1);
 	enqueue(myQueue, 2);
+	enqueue(myQueue, 3);
+	enqueue(myQueue, 4);
+
+	dequeue(myQueue);
+	dequeue(myQueue);
+	dequeue(myQueue);
+	dequeue(myQueue);
+	dequeue(myQueue);
+
+	printf("testQueue complete. (no seg fault, then all is well) \n");
+}
+
+int main(int argc, char const *argv[]) {
+	testQueue();
 
 	return 0;
 }
