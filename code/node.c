0655a23edf061549c2493a7eb76c27c9acca44d2
Christopher Stoll <stollcri@gmail.com>
2014-01-26 11:06:48 -0500

    Extract class node

---
+++ code/node.c
@@ -0,0 +1,31 @@
+/**
+ * Node Data Structure
+ * Christopher Stoll, 2014
+ */
+
+#ifndef INCLUDE_NODE
+#define INCLUDE_NODE
+
+#include <stdlib.h>
+
+/**
+ * Simple node structure
+ */
+struct node {
+	int vertex;
+	struct node *next;
+};
+
+/**
+ * Node creation function
+ */
+struct node *newNode(int vertex) {
+	struct node *thisNode = (struct node*) malloc(sizeof(struct node));
+
+	thisNode->vertex = vertex;
+	thisNode->next = NULL;
+
+	return thisNode;
+}
+
+#endif
