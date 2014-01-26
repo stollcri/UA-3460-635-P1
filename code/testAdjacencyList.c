ccd93f605d44f934f4e15e3972ab5b2c6ee3dc1a
Christopher Stoll <stollcri@gmail.com>
2014-01-26 15:11:54 -0500

    Improve the test list definition

--- code/testAdjacencyList.c
+++ code/testAdjacencyList.c
@@ -17,31 +17,20 @@ void printGraph(struct graph* currentGraph) {
 void testGraph() {
 	struct graph *myGraph = createGraph(12);
 	
-	int myList[12][2];
-	myList[0][0] = 0;
-	myList[0][1] = 1;
-	myList[1][0] = 1;
-	myList[1][1] = 2;
-	myList[2][0] = 1;
-	myList[2][1] = 3;
-	myList[3][0] = 1;
-	myList[3][1] = 4;
-	myList[4][0] = 2;
-	myList[4][1] = 5;
-	myList[5][0] = 2;
-	myList[5][1] = 6;
-	myList[6][0] = 4;
-	myList[6][1] = 7;
-	myList[7][0] = 4;
-	myList[7][1] = 8;
-	myList[8][0] = 5;
-	myList[8][1] = 9;
-	myList[9][0] = 5;
-	myList[9][1] = 10;
-	myList[10][0] = 7;
-	myList[10][1] = 11;
-	myList[11][0] = 7;
-	myList[11][1] = 12;
+	int myList[12][2] = {
+		{0, 1},
+		{1, 2},
+		{1, 3},
+		{1, 4},
+		{2, 5},
+		{2, 6},
+		{4, 7},
+		{4, 8},
+		{5, 9},
+		{5, 10},
+		{7, 11},
+		{7, 12}
+	};
 	addEdges(myGraph, 12, myList);
 	printGraph(myGraph);
 
--- code/testBreadthFirstSearch.c
+++ code/testBreadthFirstSearch.c
@@ -4,32 +4,22 @@
 void testBFS() {
 	struct graph *testGraph = createGraph(12);
 	
-	int testList[12][2];
-	testList[0][0] = 0;
-	testList[0][1] = 1;
-	testList[1][0] = 1;
-	testList[1][1] = 2;
-	testList[2][0] = 1;
-	testList[2][1] = 3;
-	testList[3][0] = 1;
-	testList[3][1] = 4;
-	testList[4][0] = 2;
-	testList[4][1] = 5;
-	testList[5][0] = 2;
-	testList[5][1] = 6;
-	testList[6][0] = 4;
-	testList[6][1] = 7;
-	testList[7][0] = 4;
-	testList[7][1] = 8;
-	testList[8][0] = 5;
-	testList[8][1] = 9;
-	testList[9][0] = 5;
-	testList[9][1] = 10;
-	testList[10][0] = 7;
-	testList[10][1] = 11;
-	testList[11][0] = 7;
-	testList[11][1] = 12;
+	int testList[12][2] = {
+		{0, 1},
+		{1, 2},
+		{1, 3},
+		{1, 4},
+		{2, 5},
+		{2, 6},
+		{4, 7},
+		{4, 8},
+		{5, 9},
+		{5, 10},
+		{7, 11},
+		{7, 12}
+	};
 	addEdges(testGraph, 12, testList);
+	bfs(testGraph, 0, 0);
 
 	printf("  ~~~~ testBFS complete. \n");
 }
