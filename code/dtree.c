b80afec0e2c10c7d0c0ece1590c504d70a2e5d3a
michaelcrouse <mec9@zips.uakron.edu>
2014-01-26 23:29:49 -0500

    expanded on BFS, added dtree to store BFS results tree

---
+++ code/dtree.c
@@ -0,0 +1,29 @@
+/**
+ * Doubly linked tree structure
+ * Used to build a search tree for BFS
+ * Michael Crouse, 1/26/14
+ */
+#ifndef INCLUDE_DTREE
+#define INCLUDE_DTREE
+ 
+#include "node.c"
+
+struct dtree {
+	struct node * data;
+	//a linked list of children
+	struct dtree *children;
+	//the next sibling to this node
+	struct dtree *next;
+	//link back to the parent
+	struct dtree *parent;
+};
+
+struct dtree * newTree(struct dtree * ancestor) {
+	struct dtree * result = (dtree *) malloc(sizeof(dtree));
+	result->data = NULL;
+	result->parent = ancestor;
+	result->children = NULL;
+	result->next = NULL;
+	return result;
+}
+#endif
