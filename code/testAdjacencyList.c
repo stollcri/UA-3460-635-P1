4c9e13296b99a5f532fc624564d76a3baf1e40ce
Christopher Stoll <stollcri@gmail.com>
2014-01-27 21:02:09 -0500

    Update to graph test
    Added data to test a

--- code/testAdjacencyList.c
+++ code/testAdjacencyList.c
@@ -7,7 +7,7 @@ void printGraph(struct graph* currentGraph) {
         
         printf("Adjacency list of vertex %d\n head", v);
         while (currentNode) {
-            printf(" -> %d", currentNode->vertex);
+            printf(" -> %d (%d)", currentNode->vertex, currentNode->costToNext);
             currentNode = currentNode->next;
         }
         printf("\n");
@@ -17,28 +17,36 @@ void printGraph(struct graph* currentGraph) {
 void testGraph() {
 	struct graph *myGraph = createGraph(1+12);
 	
-	int myList[12][2] = {
-		{0, 1},
-		{1, 2},
-		{1, 3},
-		{1, 4},
-		{2, 5},
-		{2, 6},
-		{4, 7},
-		{4, 8},
-		{5, 9},
-		{5, 10},
-		{7, 11},
-		{7, 12}
+	int myList[12][3] = {
+		{0, 1, 99},
+		{1, 2, 99},
+		{1, 3, 99},
+		{1, 4, 99},
+		{2, 5, 99},
+		{2, 6, 99},
+		{4, 7, 99},
+		{4, 8, 99},
+		{5, 9, 99},
+		{5, 10, 99},
+		{7, 11, 99},
+		{7, 12, 99}
 	};
-	addEdges(myGraph, 12, myList);
+	addEdgesWithCost(myGraph, 12, myList);
 	printGraph(myGraph);
 
 	printf("  ~~~~ testGraph complete. \n");
 }
 
+void testGraphFromFile() {
+	struct graph *myGraph = createGraphFromFile("_testList.txt");
+	printGraph(myGraph);
+
+	printf("  ~~~~ testGraphFromFile complete. \n");
+}
+
 int main(int argc, char const *argv[]) {
 	testGraph();
+	testGraphFromFile();
 	printf("~~~~~~ testAdjacencyList complete. \n");
 	return 0;
 }
