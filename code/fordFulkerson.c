eec331029f2a7e3fc13765b13a70de4bab6e32d4
Christopher Stoll <stollcri@gmail.com>
2014-01-28 20:11:49 -0500

    Mock up For-Fulkerson MaxFlow

--- code/fordFulkerson.c
+++ code/fordFulkerson.c
@@ -2,3 +2,35 @@
  * Ford-Fulkerson Algorithm
  * Christopher Stoll, 2014
  */
+
+#include "graph.c"
+//#include "breadthFirstSearch.c"
+
+int maxFlow(struct graph *thisGraph, int start, int end) {
+	int flow = 0;
+	/*
+	// initial bfs graph traversal
+	struct list *path = bfs(thisGraph, start, end);
+	// while there are still paths
+	while (path != NULL) {
+		// determine residual
+		//?????()
+
+		// flow = min(residuals)
+		//??????()
+		
+		// for each edge in the path
+		for (int i = 0; i < pathLength; ++i){
+			if(positiveFLow) {
+				positiveFlow += flow;
+			} else {
+				negativeFlow -= flow;
+			}
+		}
+		
+		// find path in residual
+		path = bfs(thisGraph, start, end);
+	}
+	*/
+	return flow;
+}
--- code/testFordFulkerson.c
+++ code/testFordFulkerson.c
@@ -1,1 +1,31 @@
 #include <stdio.h>
+#include "fordFulkerson.c"
+
+void testMaxFlow() {
+	struct graph *testGraph = createGraph(1+12);
+	
+	int testList[12][2] = {
+		{0, 1},
+		{1, 2},
+		{1, 3},
+		{1, 4},
+		{2, 5},
+		{2, 6},
+		{4, 7},
+		{4, 8},
+		{5, 9},
+		{5, 10},
+		{7, 11},
+		{7, 12}
+	};
+	addEdges(testGraph, 12, testList);
+	maxFlow(testGraph, 0, 12);
+
+	printf("  ~~~~ testMaxFlow complete. \n");
+}
+
+int main(int argc, char const *argv[]) {
+	testMaxFlow();
+	printf("~~~~~~ testFordFulkerson complete. \n");
+	return 0;
+}
