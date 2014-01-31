0ec2fa4225a895dd75c6edbb38c94ba39e20dea4
michaelcrouse <mec9@zips.uakron.edu>
2014-01-30 22:43:41 -0500

    Updated BFS - no more segfault!  Mostly behaves as expected, will need to work on graph structure next.

--- code/breadthFirstSearch.c
+++ code/breadthFirstSearch.c
@@ -36,14 +36,14 @@ struct list bfs(struct graph* thisGraph, int start, int end) {
 	//enqueue the starting node
 	//enqueue(bfsQueue, startNode->vertex);
 	enqueue(bfsQueue, bfsTree);
-	seen[start] = 1;
+	seen[startNode->vertex] = 1;
 	sinkFound=0;
 	// while there are items in the queue
 	while ((currentBranch = dequeue(bfsQueue)) != NULL) {
 	//while ((currentNode = dequeue(bfsQueue)) != NULL) {
-		printf("OUTER WHILE... %d \n", currentNode->vertex);
 		// if we have found the target
 		currentNode = currentBranch->data;
+		printf("OUTER WHILE... %d \n", currentNode->vertex);
 		if (currentNode->vertex == end) {
 			printf("END. \n");
 			sinkFound=1;
@@ -51,7 +51,6 @@ struct list bfs(struct graph* thisGraph, int start, int end) {
 		} else {
 			currentChild = NULL;
 			adjacentNode = thisGraph->adjacencyList[currentNode->vertex].head;
-			if (adjacentNode != NULL) printf("HERE AT: %d \n", adjacentNode->vertex);
 			// enqueue adjacent nodes that have not yet been seen
 			while (adjacentNode != NULL) {
 				printf("INNER WHILE... %d \n", adjacentNode->vertex);
@@ -79,16 +78,19 @@ struct list bfs(struct graph* thisGraph, int start, int end) {
 		traceStack = newStack();
 		push(traceStack, currentBranch->data);
 		do {
+			printf("TRACEBACK AT NODE %d\n", currentBranch->data->vertex);
 			currentBranch = currentBranch->parent;
 			push(traceStack, currentBranch->data);
 		} while(currentBranch != bfsTree);
 		bfsPath = newList();
 		currentNode = pop(traceStack);
 		bfsPath->head = newNode(currentNode->vertex);
+		printf("ADDING NODE %d TO LIST\n", currentNode->vertex);
 		adjacentNode = bfsPath->head;
-		while (currentNode->vertex != start) {
+		while (currentNode->vertex != end) {
 			currentNode = pop(traceStack);
 			adjacentNode->next = newNode(currentNode->vertex);
+			printf("ADDING NODE %d TO LIST\n", currentNode->vertex);
 			adjacentNode = adjacentNode->next;
 		}
 	}
