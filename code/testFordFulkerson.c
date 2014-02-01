a23afce2e1a2225cf1ee4a55bbb3139e7c4d2fc8
Christopher Stoll <stollcri@gmail.com>
2014-02-01 18:22:18 -0500

    Improve F-F test program and fix test data files

--- code/testFordFulkerson.c
+++ code/testFordFulkerson.c
@@ -14,12 +14,12 @@
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
 
-void testMaxFlowWithFile(char *fileName, int sink, int expectedFlow) {
+void testMaxFlowWithFile(char *fileName, int source, int sink, int expectedFlow) {
 	int maximumFlow = 0;
 
 	if (sink > 0) {
 		struct graph *testGraph = createGraphFromFile(fileName);
-		maximumFlow = maxFlow(testGraph, 0, sink);
+		maximumFlow = maxFlow(testGraph, source, sink);
 
 		if (maximumFlow == expectedFlow) {
 			printf("    ~~%s     OK%s, Max Flow = %d / %d (%s) \n", KGRN, KWHT, maximumFlow, expectedFlow, fileName);
@@ -32,23 +32,23 @@ void testMaxFlowWithFile(char *fileName, int sink, int expectedFlow) {
 }
 
 void testMaxFlow() {
-	testMaxFlowWithFile("testdata/testList-basic.txt", 3, 9);
-	testMaxFlowWithFile("testdata/testList-advanced.txt", 12, 26);
+	testMaxFlowWithFile("testdata/testList-basic.txt", 0, 3, 9);
+	testMaxFlowWithFile("testdata/testList-advanced.txt", 0, 12, 26);
 	
-	testMaxFlowWithFile("testdata/testList-001.txt", 5, 24);
-	testMaxFlowWithFile("testdata/testList-002.txt", 5, 999);
-	testMaxFlowWithFile("testdata/testList-003.txt", 00, 999);
-	testMaxFlowWithFile("testdata/testList-004.txt", 00, 999);
-	testMaxFlowWithFile("testdata/testList-005.txt", 00, 999);
+	testMaxFlowWithFile("testdata/testList-001.txt", 0, 5, 23);
+	testMaxFlowWithFile("testdata/testList-002.txt", 0, 3, 2000000);
+	testMaxFlowWithFile("testdata/testList-003.txt", 0, 3, 9);
+	testMaxFlowWithFile("testdata/testList-004.txt", 0, 13, 50);
+	testMaxFlowWithFile("testdata/testList-005.txt", 0, 15, 50);
 
-	testMaxFlowWithFile("testdata/v0256e1024.txt", 256, 999); 
-	testMaxFlowWithFile("testdata/v0256e2048.txt", 256, 999); 
-	testMaxFlowWithFile("testdata/v0512e2048.txt", 512, 999); 
-	testMaxFlowWithFile("testdata/v0512e4096.txt", 512, 999); 
-	testMaxFlowWithFile("testdata/v1024e4096.txt", 1024, 999); 
-	testMaxFlowWithFile("testdata/v1024e8192.txt", 1024, 999); 
-	testMaxFlowWithFile("testdata/v2048e8192.txt", 2048, 999); 
-	testMaxFlowWithFile("testdata/v2048e9999.txt", 2048, 999); 
+	testMaxFlowWithFile("testdata/v0256e1024.txt", 1, 256, 128); 
+	testMaxFlowWithFile("testdata/v0256e2048.txt", 1, 256, 360); 
+	testMaxFlowWithFile("testdata/v0512e2048.txt", 1, 510, 99); 
+	testMaxFlowWithFile("testdata/v0512e4096.txt", 1, 512, 421); 
+	testMaxFlowWithFile("testdata/v1024e4096.txt", 1, 1022, 26); 
+	testMaxFlowWithFile("testdata/v1024e8192.txt", 1, 1024, 311); 
+	testMaxFlowWithFile("testdata/v2048e8192.txt", 1, 2046, 27); 
+	testMaxFlowWithFile("testdata/v2048e9999.txt", 1, 2048, 145); 
 
 	printf("  ~~~~ testMaxFlow complete. \n");
 }
