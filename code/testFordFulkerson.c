76e3d1d3a808300bc9c4613cc909c68cfb1ab88f
Christopher Stoll <stollcri@gmail.com>
2014-02-01 01:27:44 -0500

    Remove dead code

--- code/testFordFulkerson.c
+++ code/testFordFulkerson.c
@@ -2,26 +2,6 @@
 #include "fordFulkerson.c"
 
 void testMaxFlow() {
-	/*
-	struct graph *testGraph = createGraph(1+12);
-	
-	int testList[12][2] = {
-		{0, 1},
-		{1, 2},
-		{1, 3},
-		{1, 4},
-		{2, 5},
-		{2, 6},
-		{4, 7},
-		{4, 8},
-		{5, 9},
-		{5, 10},
-		{7, 11},
-		{7, 12}
-	};
-	addEdges(testGraph, 12, testList);
-	*/
-
 	struct graph *testGraph = createGraphFromFile("testdata/testList-advanced.txt");
 	maxFlow(testGraph, 0, 12);
 
